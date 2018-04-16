b9bbc691fb1bd2ad46c32af862ab639681a14e5d
DESKTOP-7MI6HSG\steel <dame.galaxy999@gmail.com>
2018-04-16 21:48:49 +0900

    複数同じ意味のファイルがあったので消去

--- PLPK_Client/Assets/Game/Script/Base/AcEventManager.cs
+++ PLPK_Client/Assets/Game/Script/Base/AcEventManager.cs
@@ -2,15 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EventManager : MonoBehaviour {
+public class AcEventManager<T> : Singlton<AcEventManager<T>> where T : class, IEvent<T>, new()
+{    
+    public List<IEvent<T>> m_eventListenerList = new List<IEvent<T>>();
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // 消し忘れると惨事なので使用する際には細心の注意を払うこと...
+    // もう少し安全にしたいが良い方法ない？
+    public void registerEventInterFace(IEvent<T> iEvent)
+    {
+        if (iEvent == null) return;
+
+        m_eventListenerList.Add(iEvent);
+    }
+
+    public void unregisterEventInterFace(IEvent<T> iEvent)
+    {
+        if (iEvent == null) return;
+
+        m_eventListenerList.Remove(iEvent);
+    }
+
+    public void notificationEvent(T eventData)
+    {
+        foreach(var listener in m_eventListenerList)
+        {
+            listener.notificationEvent(ref eventData);
+        }
+    }
 }
